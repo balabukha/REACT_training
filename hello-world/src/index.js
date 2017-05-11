1de5336548a348316a0a2eaaca6bb53c4d2eb058
balabukha <3909591@gmail.com>
2017-05-11 10:49:53 +0300

    test 1

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -7,14 +7,43 @@ import "./index.css";
 class App extends React.Component{
     constructor(props){
         super(props);
-        this.state = {date: new Date()}
+        this.state = {
+            date: new Date(),
+            isActive: false
+        }}
+
+    //componentDidMount() - вывод в DOM
+    // componentWillUnmount() - останавливает
+
+    componentDidMount(){
+        this.timerID = setInterval(() => this.tick(), 1000)
     }
+
+    componentWillUnmount(){
+        clearInterval(this.timerID)
+    };
+
+    tick(){
+        this.setState({
+            date: new Date()
+        });
+    }
+    handleClick(){
+        this.setState({
+            isActive: !this.state.isActive
+    }
+        )
+
+        };
 
     render(){
         return(
             <div>
                 <h4>Hello World</h4>
                 <p>{this.state.date.toLocaleTimeString()}</p>
+                <button onClick={this.handleClick.bind(this)}>
+                    {this.state.isActive ? 'ON' : 'OFF'}
+                </button>
             </div>
         )
     }
